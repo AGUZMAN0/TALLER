5773ff40ea47b67bf4fd37c6aba50eee17e6dd11
AlexTaquez <alextaquez@unicomfacauca.edu.co>
2025-03-08 16:55:31 -0500

    Ejercicio3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-frase = "Esta es mi frase"
 
 
-def ejercicio2(frase):
+def ejercicio2():
+    frase = "Esta es mi frase"
     vowels = ['a', 'e', 'i', 'o', 'u']
     vocales = []
 
@@ -16,6 +16,29 @@ def ejercicio2(frase):
 
 
 
+def ejercicio3():
+    frase = "El mejor regalo? El perdón..."
+    list_frase = list(frase)
+    print(frase)
+    print(list_frase) #deben jugar con esta lista
+
+    #Aquí va la solución...
+    contador=0
+    lista = []
+    for i in list_frase:
+        if contador==0 or contador == 1 or contador == 4 and i!=".":
+            lista.append(i)
+        if i==" ":
+            contador+=1
+        
+
+    list_frase = lista
 
 
-ejercicio2(frase)
+    nueva_frase = ''.join(list_frase)
+    print(list_frase)
+    print(nueva_frase)
+
+
+ejercicio2()
+ejercicio3()
