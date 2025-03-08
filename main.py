310067307db11a9daaa74d4c0804450939f0b3e2
AGUZMAN0 <mairaguzman@unicomfacauca.edu.co>
2025-03-08 17:09:41 -0500

    tercer commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 # EJERCICIO NUMERO 4
+
 lista = [random.randint(1, 10) for _ in range (10)]
 print("Esta es la lista que se genero: " + str(lista))
 
@@ -14,6 +15,7 @@ print("El resultado de los numeros y sus potencias es:" + str (resultado));
 
 
 # EJERCICIO NUMERO 1
+
 def abecedario(n):
     let = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
     res = [let[i] for i in range(len(let)) if (i+1) % n != 0]
